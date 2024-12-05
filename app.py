532f8edab0b405f67d0db1eb913c51fed0849c19
heinkhantzaw567 <135092601+heinkhantzaw567@users.noreply.github.com>
2024-12-05 17:15:26 -0500

    Orginial

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "HEllo World"
+def hello_world():
+	return 'Hello World Again'
 
+# main driver function
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run()
