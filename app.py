5b1e1c80a18f1a5d5da96bde342196948dce0bfd
heinkhantzaw567 <135092601+heinkhantzaw567@users.noreply.github.com>
2024-12-05 17:45:51 -0500

    Test1

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World Again'
+	return render_template('index.html')
 
 # main driver function
 if __name__ == '__main__':
